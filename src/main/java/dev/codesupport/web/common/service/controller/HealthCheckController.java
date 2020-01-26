5c5558e10df5523f8b4ad601a636f12a305febcf
Atrum <krkidkyle@gmail.com>
2020-01-25 22:08:32 -0500

    Add additional swagger api annotations / information

--- src/main/java/dev/codesupport/web/common/service/controller/HealthCheckController.java
+++ src/main/java/dev/codesupport/web/common/service/controller/HealthCheckController.java
@@ -1,6 +1,8 @@
 package dev.codesupport.web.common.service.controller;
 
 import dev.codesupport.web.common.service.service.RestResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +14,10 @@ import static dev.codesupport.web.common.service.service.RestResponse.restRespon
  * Provides a simple healthcheck endpoint to check that the service is running.
  */
 @RestController
+@Api(value = "Healthcheck", description = "Healthcheck endpoint", tags = { "Healthcheck" })
 public class HealthCheckController {
 
+    @ApiOperation("Validate health of application")
     @GetMapping("/healthcheck")
     public RestResponse<Serializable> getHealthCheck() {
         return restResponse(null);

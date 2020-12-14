3cea5211e0150fc63abe9b64d310aa2007e66222
Atrum <krkidkyle@gmail.com>
2020-12-14 15:44:22 -0500

    Feature/file logging (#58)
    Add additional logging features (file/discord) and memory checks

--- src/main/java/dev/codesupport/web/common/configuration/SchedulesConfiguration.java
+++ src/main/java/dev/codesupport/web/common/configuration/SchedulesConfiguration.java
@@ -4,6 +4,7 @@ import dev.codesupport.web.common.exception.ConfigurationException;
 import dev.codesupport.web.common.service.http.client.HttpClient;
 import dev.codesupport.web.common.service.http.client.HttpMethod;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 @Configuration
 @RequiredArgsConstructor
 @EnableScheduling
+@Slf4j
 public class SchedulesConfiguration {
 
     @Value("${healthcheck.ping.url:}")
@@ -28,6 +30,14 @@ public class SchedulesConfiguration {
                     .sync(HttpMethod.GET);
         } else {
             throw new ConfigurationException("HealthCheck url not set");
+        }
+    }
+
+    @Scheduled(initialDelay = 60000, fixedDelay = 30000)
+    public void scheduleMemoryCheck() {
+        double freeMemory = (double)Runtime.getRuntime().freeMemory() / Runtime.getRuntime().totalMemory();
+        if (freeMemory < 0.25) {
+            log.warn("Running low on memory: " + ((int)(freeMemory * 100)) + "%");
         }
     }
 
